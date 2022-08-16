726d3ad255f1c89a13e5d6971b5c5ea9f95859dd
bnbn332 <bnbn332@naver.com>
2022-08-16 09:30:14 +0900

    nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter path={process.env.PUBLIC_URL}>
+      <Navigation />
       <Routes>
         <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
         <Route path="/movie/:id" element={<Detail />} />
